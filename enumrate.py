000765817cb82b49795a83bcab2e3b794e430f75
Emmanuelsaleemgit <emmanuelsaleem098765@gmail.com>
2023-11-05 14:24:49 +0500

    enumerate function

---
+++ enumrate.py
@@ -0,0 +1,10 @@
+print("enumrate function")
+
+num = [1,2,3,4,5,6,7,8,9,0]
+
+# for i in num:
+#     print("value of num : ",i )
+
+# get index use enumrate
+for index,i in enumerate(num):
+    print("value of num : ",i,"and the index of loop is : ",index )
