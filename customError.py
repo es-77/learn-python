d441d08fcfc2a6e3f788f61a2f48898b97e93f00
Emmanuelsaleemgit <emmanuelsaleem098765@gmail.com>
2023-11-05 13:42:39 +0500

    custom error

---
+++ customError.py
@@ -0,0 +1,10 @@
+print('custom error')
+
+name = input("Enter name : ")
+
+if(name == "Emmanuel"):
+    raise ValueError('Emmanuel is block')
+else:
+    print("you can go")    
+
+print("program end")    
