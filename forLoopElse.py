c1464b6fd247fcc9c8410e9e7737f057196f7a12
Emmanuelsaleemgit <emmanuelsaleem098765@gmail.com>
2023-11-05 12:41:45 +0500

    loop with  else

---
+++ forLoopElse.py
@@ -0,0 +1,31 @@
+print('for loop with else condition')
+
+# # go in loop
+# for i in [1,2,3,4]:
+#     print("loop in",1)
+# else:
+#     print('else condition')    
+# # go in loop else condition
+# for i in []:
+#     print("loop in",1)
+# else:
+#     print('else condition')    
+# # after break not go in else condition
+# for i in range(6):
+#     if(i == 0):
+#         break
+#     print("loop in",1)
+# else:
+#     print('else condition')    
+
+# else with while"
+# i = 0
+# while i < 7:
+#     # if(i == 0):
+#     #     break
+#     print("loop in",1)
+#     i = i+1
+# else:
+#     print('else condition')    
+
+# print("after loop")
