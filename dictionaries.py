4127fcf96f20541799085f3d9c6716b383067fe9
Emmanuelsaleemgit <emmanuelsaleem098765@gmail.com>
2023-11-05 12:29:38 +0500

    dictionaries and there method

--- dictionaries.py
+++ dictionaries.py
@@ -1,1 +1,5 @@
 print("dictionaries python")
+dic = {'name':"emmanuel",'last_name':"saleem"}
+print(dic,type(dic),dic['name'],dic.get('name'))
+print(dic.values(),dic.keys())
+print(dic.items(),"item data")
---
+++ dictionariesMethods.py
@@ -0,0 +1,22 @@
+print("dictionaries methods")
+
+num1 = {1:1,2:2,3:3,4:4,5:5}
+num2 = {6:6,7:7,8:8,9:9,0:0}
+# udate dictionary
+# num1.update(num2)
+# print(num1)
+# clear dictionary
+# num1.clear()
+# print(num1)
+# # remove one item dictionary
+# num1.pop(1)
+# print(num1)
+# # remove last item dictionary
+# num1.popitem()
+# print(num1)
+# remove variable item dictionary
+# del num1
+# print(num1)
+# remove one item dictionary
+del num1[1]
+print(num1)
