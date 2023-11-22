0a27ceec539cfffcc81fd6cb7b0b40e07a35cb4e
Emmanuelsaleemgit <emmanuelsaleem098765@gmail.com>
2023-11-23 01:06:12 +0500

    methodOverriding

--- opps/methodOverriding.py
+++ opps/methodOverriding.py
@@ -1,1 +1,26 @@
 print("methodOverriding.py")
+
+class Parent:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def arear(self):
+        return self.x * self.y    
+    def myMethod(self):
+        return "my method parent"
+
+
+class Child(Parent):
+    def childMethod(self):
+        return "child method run"
+
+    def circleArear(self):
+        return  3.14 * super().arear()   
+
+
+
+child = Child(2,2)
+
+print(child.arear())
+print(child.circleArear())
